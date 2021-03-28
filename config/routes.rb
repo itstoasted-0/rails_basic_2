f27e197d88ea2592a7322c1d269eb8b845ae06f1
itstoasted-0 <snow.w686@gmail.com>
2021-03-28 09:45:26 +0900

    一覧表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
 end
